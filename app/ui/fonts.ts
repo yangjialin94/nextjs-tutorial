7e2af50fe32ac1eff242c0d6fdca7c7d9ff80bf7
Jialin Yang <19481802+yangjialin94@users.noreply.github.com>
2025-02-14 17:43:58 -0500

    Resolved build issue

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Caveat, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'] ,
+  weight: ['400'],
+});
+export const caveat = Caveat({
+  subsets: ['latin'] ,
+  weight: ['700'],
+});
